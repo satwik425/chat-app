4a5d7df03be5d3f9d441e5559521a149ff443d6d
satwik425 <satvikshekhar20@gmail.com>
2026-04-28 15:28:28 +0530

    updted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-app-7iby.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
